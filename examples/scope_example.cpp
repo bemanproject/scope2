be25679dd4941c0a3907aab34a535becda31c928
Robert Andrzejuk <robert.andrzejuk@infor.com>
2025-02-05 19:22:35 +0100

    Revert "Fix formatting to pass with checks"
    This reverts commit 894f73babeaf213f85826f2d511ff6c14af130cf.

--- examples/scope_example.cpp
+++ examples/scope_example.cpp
@@ -5,30 +5,26 @@
 #include <iostream>
 #include <string_view>
 
-void print_exit_status(std::string_view name, bool exit_status, bool did_throw)
-{
+void print_exit_status(std::string_view name, bool exit_status, bool did_throw) {
     std::cout << name << ":\n";
-    std::cout << "  Exception thrown: " << (did_throw ? "yes" : "no") << "\n";
-    std::cout << "  Exit status:      " << (exit_status ? "finished" : "pending") << "\n\n";
+    std::cout << "  Throwed exception  " << (did_throw ? "yes" : "no") << "\n";
+    std::cout << "  Exit status        " << (exit_status ? "finished" : "pending") << "\n\n";
 }
 
 // Randomly throw an exception (50% chance)
-void maybe_throw()
-{
+void maybe_throw() {
     if (std::rand() >= RAND_MAX / 2)
         throw std::exception{};
 }
 
-int main()
-{
+int main() {
     bool exit_status{false}, did_throw{false};
 
     // Manual handling at "end of scope"
     try {
         maybe_throw();
         exit_status = true;
-    }
-    catch (...) {
+    } catch (...) {
         did_throw = true;
     }
     print_exit_status("Manual handling", exit_status, did_throw);
@@ -38,8 +34,7 @@ int main()
     try {
         auto guard = std::experimental::scope_exit{[&] { exit_status = true; }};
         maybe_throw();
-    }
-    catch (...) {
+    } catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_exit", exit_status, did_throw);
@@ -49,8 +44,7 @@ int main()
     try {
         auto guard = std::experimental::scope_fail{[&] { exit_status = true; }};
         maybe_throw();
-    }
-    catch (...) {
+    } catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_fail", exit_status, did_throw);
@@ -60,8 +54,7 @@ int main()
     try {
         auto guard = std::experimental::scope_success{[&] { exit_status = true; }};
         maybe_throw();
-    }
-    catch (...) {
+    } catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_success", exit_status, did_throw);
