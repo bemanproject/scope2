8eafa0cb545cfaa6aedee2b88ff0560943cafae5
Jeff Garland <jeff@crystalclearsoftware.com>
2025-02-04 18:14:43 -0700

    make empty scope header

--- include/beman/scope/scope.hpp
+++ include/beman/scope/scope.hpp
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#ifndef BEMAN_SCOPE_HPP
+#define BEMAN_SCOPE_HPP
+
+
+namespace beman::scope {
+
+
+} // namespace beman::scope
+
+#endif // BEMAN_SCOPE_HPP
