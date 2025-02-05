894f73babeaf213f85826f2d511ff6c14af130cf
Robert Andrzejuk <robert.andrzejuk@infor.com>
2025-02-05 17:16:26 +0100

    Fix formatting to pass with checks

--- examples/scope_example.cpp
+++ examples/scope_example.cpp
@@ -8,8 +8,8 @@
 void print_exit_status(std::string_view name, bool exit_status, bool did_throw)
 {
     std::cout << name << ":\n";
-    std::cout << "  Throwed exception  " << (did_throw ? "yes" : "no") << "\n";
-    std::cout << "  Exit status        " << (exit_status ? "finished" : "pending") << "\n\n";
+    std::cout << "  Exception thrown: " << (did_throw ? "yes" : "no") << "\n";
+    std::cout << "  Exit status:      " << (exit_status ? "finished" : "pending") << "\n\n";
 }
 
 // Randomly throw an exception (50% chance)
@@ -24,52 +24,44 @@ int main()
     bool exit_status{false}, did_throw{false};
 
     // Manual handling at "end of scope"
-    try
-    {
+    try {
         maybe_throw();
         exit_status = true;
     }
-    catch (...)
-    {
+    catch (...) {
         did_throw = true;
     }
     print_exit_status("Manual handling", exit_status, did_throw);
 
     // Using scope_exit: runs on scope exit (success or exception)
     exit_status = did_throw = false;
-    try
-    {
+    try {
         auto guard = std::experimental::scope_exit{[&] { exit_status = true; }};
         maybe_throw();
     }
-    catch (...)
-    {
+    catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_exit", exit_status, did_throw);
 
     // Using scope_fail: runs only if an exception occurs
     exit_status = did_throw = false;
-    try
-    {
+    try {
         auto guard = std::experimental::scope_fail{[&] { exit_status = true; }};
         maybe_throw();
     }
-    catch (...)
-    {
+    catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_fail", exit_status, did_throw);
 
     // Using scope_success: runs only if no exception occurs
     exit_status = did_throw = false;
-    try
-    {
+    try {
         auto guard = std::experimental::scope_success{[&] { exit_status = true; }};
         maybe_throw();
     }
-    catch (...)
-    {
+    catch (...) {
         did_throw = true;
     }
     print_exit_status("scope_success", exit_status, did_throw);
