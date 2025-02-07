1a55b425f6a56361b69676e2b36c972df731fd93
Robert Andrzejuk <robert.andrzejuk@infor.com>
2025-02-07 19:07:06 +0100

    Copy synopsis from p0052r10.pdf

--- include/beman/scope/scope.hpp
+++ include/beman/scope/scope.hpp
@@ -5,6 +5,7 @@
 
 namespace beman::scope {
 
+<<<<<<< HEAD
 // TODO: Implement
 struct scope_exit {
     template <typename F>
@@ -31,6 +32,95 @@ struct scope_success {
         // TODO: Cleanup
     }
 };
+=======
+// -- 7.6.7 Feature test macro --
+//
+//        __cpp_lib_scope
+//
+
+// -- 7.5.1 Header <scope> synopsis [scope.syn] --
+//
+//        namespace std {
+//        template <class EF>
+//        class scope_exit;
+//
+//        template <class EF>
+//        class scope_fail;
+//
+//        template <class EF>
+//        class scope_success;
+//
+//        template <class R, class D>
+//        class unique_resource;
+//
+//        // factory function
+//        template <class R, class D, class S = decay_t<R>>
+//        unique_resource<decay_t<R>, decay_t<D>>
+//        make_unique_resource_checked(R&& r, const S& invalid, D&& d) noexcept(see below);
+//        } // namespace std
+//
+
+// -- 7.5.2 Scope guard class templates [scope.scope_guard] --
+//
+//        template <class EF>
+//        class scope_guard {
+//        public:
+//            template <class EFP>
+//            explicit scope_guard(EFP&& f) noexcept(see below);
+//
+//            scope_guard(scope_guard&& rhs) noexcept(see below);
+//
+//            scope_guard(const scope_guard&)            = delete;
+//            scope_guard& operator=(const scope_guard&) = delete;
+//            scope_guard& operator=(scope_guard&&)      = delete;
+//
+//            ~scope_guard() noexcept(see below);
+//
+//            void release() noexcept;
+//
+//        private:
+//            EF   exit_function;                               // exposition only
+//            bool execute_on_destruction{true};                // exposition only
+//            int  uncaught_on_creation{uncaught_exceptions()}; // exposition only
+//        };
+//
+//        template <class EF>
+//        scope_guard(EF) -> scope_guard<EF>;
+//
+
+// -- 7.6.1 Class template unique_resource [scope.unique_resource.class] --
+//
+//        template <class R, class D>
+//        class unique_resource {
+//        public:
+//            unique_resource();
+//            template <class RR, class DD>
+//            unique_resource(RR&& r, DD&& d) noexcept(see below);
+//            unique_resource(unique_resource&& rhs) noexcept(see below);
+//            ~unique_resource();
+//            unique_resource& operator=(unique_resource&& rhs) noexcept(see below);
+//            void             reset() noexcept;
+//            template <class RR>
+//            void      reset(RR&& r);
+//            void      release() noexcept;
+//            const R&  get() const noexcept;
+//            see below operator*() const noexcept;
+//            R         operator->() const noexcept;
+//            const D&  get_deleter() const noexcept;
+//
+//        private:
+//            using R1 = conditional_t<is_reference_v<R>,                        //
+//                                    reference_wrapper<remove_reference_t<R>>, //
+//                                    R>;                                       // exposition only
+//
+//            R1   resource;               // exposition only
+//            D    deleter;                // exposition only
+//            bool execute_on_reset{true}; // exposition only
+//        };
+//
+//        template <typename R, typename D>
+//        unique_resource(R, D) -> unique_resource<R, D>;
+>>>>>>> a30545a (Copy synopsis from p0052r10.pdf)
 
 } // namespace beman::scope
 
